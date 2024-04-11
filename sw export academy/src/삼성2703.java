0a5d11dbb8aeabf2bfd23fb1f4f741d12965bade
yooniverse7 <tpdbs0701@gmail.com>
2024-04-11 14:59:34 +0900

    홀수만 더하기

--- sw export academy/src/삼성2703.java
+++ sw export academy/src/삼성2703.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class 삼성2703 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = sc.nextInt();
     }
 }
