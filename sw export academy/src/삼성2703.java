089dc91e7e1e2bb0b58ea1da28a464fed763da76
yooniverse7 <tpdbs0701@gmail.com>
2024-04-11 16:06:47 +0900

    홀수만 더하기

--- sw export academy/src/삼성2703.java
+++ sw export academy/src/삼성2703.java
@@ -5,5 +5,34 @@ public class 삼성2703 {
         Scanner sc = new Scanner(System.in);
         int T;
         T = sc.nextInt();
+        int[][] arr = new int[T][10];
+        int[] result = new int[T];
+        
+
+        for (int i = 0; i < T; i++) {  // 배열에 추가하기
+            for (int j = 0; j<10; j++) {
+                int a = sc.nextInt();
+                arr[i][j] = a;
+            }
+        }
+
+        for (int i = 0; i < T; i++) {
+            int sum = 0;
+            for (int j = 0; j<10; j++) {
+                if (arr[i][j]%2 != 0) { //홀수만 더하기
+
+                    sum += arr[i][j];
+                }
+                result[i] = sum;
+            }
+        }
+
+        for (int i = 0; i < T; i++) { //출력하기
+            System.out.println("#"+ (i+1) + " " + result[i]);
+        }
+        
+        sc.close();
+
+
     }
 }
