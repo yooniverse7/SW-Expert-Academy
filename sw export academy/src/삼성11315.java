e3d171b6117e8e2801eed8bffb2ccfcdffd562a9
yooniverse7 <tpdbs0701@gmail.com>
2024-05-09 19:19:00 +0900

    오목판정

--- sw export academy/src/삼성11315.java
+++ sw export academy/src/삼성11315.java
@@ -3,29 +3,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class 삼성11315 {
-    static int[][] checkArr = {{0,1},{1,0},{1,-1},{-1,1}}; // 가로, 세로, 왼쪽 하강, 오른쪽 하강
-    static String[][] arr;
-    static int N;
+    static int[][] checkArr = {{0,1},{1,-1},{1,0},{1,1}}; // 가로, 세로, 왼쪽 하강, 오른쪽 하강
+    static char[][] arr;
+    public static int N;
 
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine()); // 테스트 케이스
 
         for(int tc = 1; tc<=T; tc++) {
-            int N = Integer.parseInt(br.readLine());
-            arr = new String[N][N];
+            N = Integer.parseInt(br.readLine()); // 오목판
+            arr = new char[N][N];
 
-            for(int i = 0; i<N; i++) { // 배열에 담기
-                arr[i] = br.readLine().split("");
+            for(int i=0; i<N; i++){
+                String line = br.readLine();
+                for(int j=0; j<N; j++){
+                    arr[i][j] = line.charAt(j);
+                }
             }
 
             boolean check5 = false;
             for(int x = 0; x<N; x++) {
                 for(int y = 0; y<N; y++) {
-                    if(arr[x][y].equals("o")) {
+                    if(arr[x][y] == 'o') {
                         for(int c = 0; c<4; c++) {
                             int cnt = check(x, y, c);
-
                            // System.out.println(cnt);
                             if(cnt == 5) {
                                 check5 = true;
@@ -36,11 +38,11 @@ public class 삼성11315 {
                 }
             }
 
-            // if(check5 == true) {
-            //     System.out.println("#" + tc + " " + "YES");
-            // }else {
-            //     System.out.println("#" + tc + " " + "NO");
-            // }
+            if(check5 == true) {
+                System.out.println("#" + tc + " " + "YES");
+            }else {
+                System.out.println("#" + tc + " " + "NO");
+            }
 
         }
 
@@ -50,15 +52,13 @@ public class 삼성11315 {
         int cnt = 1;
         int cx = x;
         int cy = y;
-        // System.out.println(x + " " + y);
-        for(int i = 1; i<=4; i++) {
+        cute : for(int i = 1; i<=4; i++) {
             cx = cx + checkArr[c][0];
             cy = cy + checkArr[c][1];
-            System.out.println(cx + " " + cy);
-            if( cx>=0 && cx<N && cy>=0 && cy<N && arr[cx][cy].equals("o")) {
+            if(cx>=0 && cx<N && cy>=0 && cy<N && arr[cx][cy] == 'o') {
                 cnt++;
             }else {
-                break;
+                break cute;
             }
         }
         return cnt;
